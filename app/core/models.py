c27d7e25cb7871c652d5481e2525cd40d1645df7
Daniel Ale <danielale9291@gmail.com>
2019-08-13 09:57:30 +0100

    Fix the unused import in admin

--- app/core/models.py
+++ app/core/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,13 @@ class ModelTests(TestCase):
         """Test creating a new user with email is successful"""
         email = "test@daniel.com"
         password = "test12344"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(email=email,
+                                                    password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test that a new user email is normalized"""
+        email = "test@daniel.COM"
+        user = get_user_model().objects.create_user(email, "test1234")
+        self.assertEqual(user.email, email.lower())
